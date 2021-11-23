a194bc83f0e3ab0e83b3ffb141632749b8c2f505
Софья Нильга <nlgs1337@gmail.com>
2021-11-23 13:06:06 +0200

    random object commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bot = new Telegraf('2082509203:AAEiiUsgrLeiXLX4q5AODpJpQD1n3N1ub-0');
 
 bot.command('start', ctx => {
     console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to Cinemaland bot. To book a ticket choose  /ticket',{
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to The Metropolian museum bot. If you want choose departments our museum press  /departments',{
     })
 })
 
@@ -26,27 +26,69 @@ bot.command('departments', ctx => {
     })
 })
 
-// bot.action( department.departmentId, ctx => {
-//     console.log(ctx.from)
-//     let objects=[];
-//     axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='+departmentId).then((response)=>{
-//         objects = response.data.objects;
-//        console.log(response.data)
-//     })
-// })
-
 bot.on('callback_query', query =>{
+    const btn = {
+        "reply_markup": JSON.stringify({
+            "inline_keyboard":[
+                [
+                    {
+                    text: "More",
+                    callback_data: "More"                            
+                    },
+                    {
+                        text: "Next",
+                        callback_data: "Next"                            
+                    }
+                ],
+            ]
+        })
+    };
+    let objects=[];
+    let object=[];
     console.log(query)
     const id = query.update.callback_query.message.chat.id;
     axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='+query.update.callback_query.data).then((response)=>{
-        console.log(response.data) //тут всі id обєктів обраного відділу
-        let objects = response.data;
-      //  const result = departments.filter(item => {return item.displayName === })[0];
-        let md = 'всього у відділі експонатів ' + response.data.total; 
-        bot.telegram.sendMessage(id, md, {parse_mode:'markdown'})
+        objects = response.data; 
+        // console.log(objects)
+        const randomId = response.data.objectIDs[Math.floor(Math.random() * response.data.objectIDs.length)];    
+        axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+randomId).then((response)=>{
+            // console.log(response.data)
+            object = response.data;
+
+            if(object.primaryImage == ''){
+                object.primaryImage = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png'
+            } 
+            if(object.artistDisplayName == ''){
+                object.artistDisplayName = 'unknown'
+            } 
+            if(object.title == ''){
+                object.title = object.objectName
+            } 
+            if(object.objectDate == ''){
+                object.objectDate = 'unknown'
+            } 
+            if(object.country == ''){
+                object.country = 'unknown'
+            }
+
+            let img = object.primaryImage;
+            let md = `
+                Name artwork: ${object.title}
+Artist: ${object.artistDisplayName}
+Date when an artwork was created: ${object.objectDate} 
+Country where created: ${object.geographyType} ${object.country} 
+`; 
+
+            bot.telegram.sendPhoto(id, img, {parse_mode:'markdown'}) 
+            bot.telegram.sendMessage(id, md, {parse_mode:'markdown'})
+            bot.telegram.sendMessage(id, btn, {parse_mode:'markdown'})             
         })
-    
+
+
+    }) 
 })
+
+
 // bot.command('ticket',ctx=> {
 //     console.log(ctx.from)
 //     let ticketMessage = 'great, choose the day of week for which you want to take a ticket';
@@ -63,346 +105,12 @@ bot.on('callback_query', query =>{
 //         })
 // })
 
-bot.command('cinema', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Today the following cinemas cooperate with our bot:  Dovzhenko cinema ',{
-    })
-})
-
-
-
 bot.command('help',ctx=> {
     console.log(ctx.from)
     ctx.deleteMessage();
     bot.telegram.sendMessage(ctx.chat.id,'info',{})
        
 })
-
-bot.action('Dovzhenko cinema',ctx => {
-    console.log(ctx.from)    
-    bot.telegram.sendMessage(ctx.chat.id,'Great, now you can view the film',{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Thursday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Friday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Sunday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Monday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Saturday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Tuesday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Wednesday',ctx => {
-    console.log(ctx.from)
-    let filmMessage = 'great, choose the film';
-    bot.telegram.sendMessage(ctx.chat.id,filmMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "Film1",
-                        callback_data: 'Film1'
-                    }
-                ],
-                [
-                    {
-                        text: "Film2",
-                        callback_data: 'Film2'
-                    }
-                ],
-                [
-                    {
-                        text: "Film3",
-                        callback_data: 'Film3'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Film1',ctx => {
-    console.log(ctx.from)
-    let timeMessage = 'great, choose the time';
-    bot.telegram.sendMessage(ctx.chat.id,timeMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "09.00 a.m.",
-                        callback_data: '9am'
-                    }
-                ],
-                [
-                    {
-                        text: "01.00 p.m.",
-                        callback_data: '1pm'
-                    }
-                ],
-                [
-                    {
-                        text: "10.00 p.m.",
-                        callback_data: '10pm'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Film2',ctx => {
-    console.log(ctx.from)
-    let timeMessage = 'great, choose the time';
-    bot.telegram.sendMessage(ctx.chat.id,timeMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "08.30 a.m.",
-                        callback_data: '8am'
-                    }
-                ],
-                [
-                    {
-                        text: "02.15 p.m.",
-                        callback_data: '2pm'
-                    }
-                ],
-                [
-                    {
-                        text: "08.00 p.m.",
-                        callback_data: '8pm'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('Film3',ctx => {
-    console.log(ctx.from)
-    let timeMessage = 'great, choose the time';
-    bot.telegram.sendMessage(ctx.chat.id,timeMessage,{
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: "10.20 a.m.",
-                        callback_data: '10am'
-                    }
-                ],
-                [
-                    {
-                        text: "05.05 p.m.",
-                        callback_data: '5pm'
-                    }
-                ],
-                [
-                    {
-                        text: "11.00 p.m.",
-                        callback_data: '11pm'
-                    }
-                ]
-            ]
-        }
-      })
-})
-
-bot.action('10pm', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'please, enter the number of tickets',{
-    })
-})
-
-
 
 //---------------------------------------------------------------------
 
